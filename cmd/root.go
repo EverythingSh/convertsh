fa00697b158fb27122f733f8697c4f0beb70d1ac
Ansuman Sahoo <anshumansahoo500@gmail.com>
2024-12-23 18:35:33 +0530

    add a forgotten file
    Signed-off-by: Ansuman Sahoo <anshumansahoo500@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -12,13 +9,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "con",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Convert any file to any format",
+	Long:  `Convert any file to any format. For example:`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
